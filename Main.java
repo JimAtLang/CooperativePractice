bf264a105ac66425c6a2d3eb48d9d34959fe7113
Jim Stewart <jim.stewart@thelangschool.org>
2025-03-26 11:13:53 -0400

    combiner class:

--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
 public class Main {
+    public static void main(String[] args){
+        new Main();
+    }
 
+    public Main(){
+        String s = "axbxvccxadx";
+        int n = 10000;
+        Counter c = new Counter();
+        Divider d = new Divider();
+        Modulator m = new Modulator();
+        Repeater r = new Repeater();
+        Combiner cm = new Combiner();
+
+        int count = c.count(s,n);
+        int quot = d.divide(s,n);
+        int prod = r.repeat(count, quot);
+        int rem = m.modulate(s, n);
+        int slice = c.count(s,rem);
+        int total = cm.combine(rem, slice);
+        System.out.println(total);
+    }
 }
